55dc9fb0b22e18b223b0f2c40e0ca0b3b2d3654c
Breyer Guillaume <breyerguillaume@gmail.com>
2023-02-21 11:54:58 +0100

    endCategorie

--- src/components/list-category/categorie.jsx
+++ src/components/list-category/categorie.jsx
@@ -2,7 +2,7 @@ import styles from './categorie.module.css'
 
 const Categorie = (props) => {
     // ci dessous transformer l'heure
-    // const heureMess = 
+    const heureMess = `${new Date(props.data.lastMess).getHours()}h${new Date(props.data.lastMess).getMinutes()}`
 
     return (
         <li onClick={()=>{props.choixCat(props.data.id)}} className={styles.liste}>
@@ -10,8 +10,8 @@ const Categorie = (props) => {
                 <p className={styles.nom} >{props.data.nom}</p> 
                 <p className={styles.message}>{props.data.nbreMess} message(s) </p> 
             </div>
-            <div>
-                <p className={styles.heure}>Dernier message : {props.data.lastMess} </p>
+            <div className={styles.ctnHeure} >
+                <p className={styles.heure}>Dernier message : {heureMess} </p>
             </div>
             
         </li>
--- src/components/list-category/list-category.jsx
+++ src/components/list-category/list-category.jsx
@@ -1,7 +1,7 @@
 import styles from './list-category.module.css'
 import Categorie from './categorie'
 
-const Data= [{id:1, nom:'React', nbreMess: 12, lastMess : Date()},{id:2, nom:'React 2', nbreMess: 2, lastMess : Date()}]
+const Data= [{id:1, nom:'React', nbreMess: 12, lastMess : 1107110465663},{id:2, nom:'React 2', nbreMess: 2, lastMess : Date()}]
 
 const ListCategory = (props) => {
     return (
