236a6e3609ec165bbc8b13423160b9a06c53b0d5
Breyer Guillaume <breyerguillaume@gmail.com>
2023-02-21 10:23:25 +0100

    update category

--- src/components/list-category/list-category.jsx
+++ src/components/list-category/list-category.jsx
@@ -1,7 +1,16 @@
 import styles from './list-category.module.css'
+import Categorie from './categorie'
 
+const Data= [{nom:'react', nbreMess: 12, lastMess : Date()}]
 
-const ListCategory = (props) => {}
-
+const ListCategory = (props) => {
+    return (
+        <div>
+            <ul>
+                {Data.map(cat=><Categorie data={cat} />)}
+            </ul>
+        </div>
+    )
+}
 
 export default ListCategory
