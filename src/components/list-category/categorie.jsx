49f6d264332299eefb8cb568275c4a8959875b4b
Breyer Guillaume <breyerguillaume@gmail.com>
2023-02-21 10:23:04 +0100

    update category

---
+++ src/components/list-category/categorie.jsx
@@ -0,0 +1,11 @@
+import styles from './categorie.module.css'
+
+const Categorie = ({nom,nbreMess,lastMess}) => {
+    return (
+        <li>
+            
+        </li>
+    )
+}
+
+export default Categorie
