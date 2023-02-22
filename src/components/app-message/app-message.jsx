30520f4f890b07d9bf97c81e4659443943ffbbfd
rossitto23 <49282@etu.he2b.be>
2023-02-22 15:06:28 +0100

    test

--- src/components/app-message/app-message.jsx
+++ src/components/app-message/app-message.jsx
@@ -1,34 +1,33 @@
 import { useState } from "react";
 import ListCategory from "../list-category/list-category";
 import Login from "../login/login";
-import styles from "./app-message.module.css"
-
+import styles from "./app-message.module.css";
 
 const AppMessage = () => {
-    const [choixCategorie, setChoixCategorie] = useState(null)
-    const categorySelected = (id) => {
-        setChoixCategorie(id)
-        console.log(id);
-    }
-    
-    const [logged, setLogged] =  useState(false)
-    const login = () => {
-        setLogged(true)
-}
+  const [choixCategorie, setChoixCategorie] = useState(null);
+  const categorySelected = (id) => {
+    setChoixCategorie(id);
+    console.log(id);
+  };
 
-    return(
-        <div className={styles.border}>
-            <div className={styles.main} >
-                <div className={styles.ctn} >
-                <h1 className={styles.titre} >Message App</h1>
-                </div>
-                    {/* <Login onLog = {login}/> */}
-                    {logged && <ListCategory onCat = {categorySelected}/>}
-                    {/* {choixCategorie && <ListMessageCategory id={choixCategorie}/>} */}
-                    {/* <ListMessageCategory /> */}
-            </div>
+  const [logged, setLogged] = useState(null);
+  const login = () => {
+    setLogged(true);
+  };
+
+  return (
+    <div className={styles.border}>
+      <div className={styles.main}>
+        <div className={styles.ctn}>
+          <h1 className={styles.titre}>Message App</h1>
         </div>
-    )
-}
+        {/* <Login onLog = {login}/> */}
+        {logged && <ListCategory onCat={categorySelected} />}
+        {/* {choixCategorie && <ListMessageCategory id={choixCategorie}/>} */}
+        {/* <ListMessageCategory /> */}
+      </div>
+    </div>
+  );
+};
 
 export default AppMessage;
